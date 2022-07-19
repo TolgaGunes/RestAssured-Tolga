9e9e8e3203c9a180ef186802d869214dfeb2cac9
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-19 19:46:18 +0200

    Day3: pathParam tests

--- src/test/java/com/cybertek/day3/SpartanTestWithParameters.java
+++ src/test/java/com/cybertek/day3/SpartanTestWithParameters.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
@@ -36,6 +37,33 @@ public class SpartanTestWithParameters {
 
         // verify Blythe in the json payload/body
         assertTrue(response.body().asString().contains("Blythe"));
+    }
+
+    @DisplayName("GET request to /api/spartan{id}")
+    @Test
+    public void test2(){
+        Response response = given()
+                .accept(ContentType.JSON).and().pathParam("id", 500).
+                when().get("/api/spartan/{id}");
+
+        assertEquals(404,response.statusCode());
+        assertEquals("application/json", response.getContentType());
+        assertEquals(response.body().asString().contains("Not Found"), true);
+
+    }
+
+    @DisplayName("GET request to /api/spartans/search")
+    @Test
+    public void test3() {
+
+        Response response = given().log().all().accept(ContentType.JSON)
+                .and().queryParam("nameContains", "e")
+                .and().queryParam("gender", "Female")
+                .when().get("/api/spartans/search");
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json", response.contentType());
+        assertTrue(response.body().asString().contains("Female"));
+        assertTrue(response.body().asString().contains("Janette"));
 
     }
 }
