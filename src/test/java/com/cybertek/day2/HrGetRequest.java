f6fa90172bf6952844c4e57a2ed267bd7dfa6db9
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-19 10:50:49 +0200

    Day2-Hr getreqest task done

--- src/test/java/com/cybertek/day2/HrGetRequest.java
+++ src/test/java/com/cybertek/day2/HrGetRequest.java
@@ -1,20 +1,54 @@
 package com.cybertek.day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HrGetRequest {
 
     //BeforeAll is an annotation equals to @BeforeClass in TestNg, we use static method name
     @BeforeAll
     public static void init() {
-        RestAssured.baseURI = "http://3.80.210.194:1000/ords/hr";
-
-
-
+        // save baseurl inside this variable so that we don't need to type each https method
+         baseURI = "http://3.80.210.194:1000/ords/hr";
 
     }
 
+    @DisplayName(("GET request to / regions"))
+    @Test
+    public void test1() {
+        Response response = get("/regions");
+        System.out.println(response.statusCode());
+    }
+
+    @DisplayName("GET request to /regions/2")
+    @Test
+    public void test2() {
+
+        Response response = given().accept(ContentType.JSON)
+                .when()
+                .get("/regions/2");
+        assertEquals(200, response.statusCode());
+
+        //verify content type
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+
+        //verify body contains Americas
+
+      //  Assertions.assertEquals(response.body().asString().contains("Americas"), true);
+         Assertions.assertTrue(response.body().asString().contains("Americas"));
+
+    }
 
 
 }
