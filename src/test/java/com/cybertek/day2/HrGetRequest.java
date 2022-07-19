11287e2d9a22586cdf86aac63113d9c189d8d6fb
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-19 10:18:27 +0200

    Day2-third commit

--- src/test/java/com/cybertek/day2/HrGetRequest.java
+++ src/test/java/com/cybertek/day2/HrGetRequest.java
@@ -1,5 +1,20 @@
 package com.cybertek.day2;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
 public class HrGetRequest {
+
+    //BeforeAll is an annotation equals to @BeforeClass in TestNg, we use static method name
+    @BeforeAll
+    public static void init() {
+        RestAssured.baseURI = "http://3.80.210.194:1000/ords/hr";
+
+
+
+
+    }
+
+
 
 }
