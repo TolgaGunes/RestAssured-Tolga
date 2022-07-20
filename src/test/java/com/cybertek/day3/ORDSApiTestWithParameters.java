283f98d44456afbad8ad3cdf52db16dd50e89d72
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-20 17:03:32 +0200

    "ORDS APi Test_with QueryParam"

--- src/test/java/com/cybertek/day3/ORDSApiTestWithParameters.java
+++ src/test/java/com/cybertek/day3/ORDSApiTestWithParameters.java
@@ -19,20 +19,40 @@ public class ORDSApiTestWithParameters {
 
     }
 
-    @DisplayName("Get request to /countries")
+    @DisplayName("Get request to /countries with Query Param")
     @Test
     public void test1() {
-        Response response= given().accept(ContentType.JSON).and().queryParam("q","{\"region_id\": 3}").
-        log().all()
+        Response response= given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"region_id\":2}").
+                    log().all()
                 .when()
-                .accept(ContentType.JSON).get(baseURI);
+                .get("/countries");
 
 
-        assertEquals(200, response.statusCode());
+       assertEquals(200, response.statusCode());
         assertEquals("application/json",response.header("Content-Type"));
         assertTrue(response.body().asString().contains("United States of America"));
 
+       response.prettyPrint();
+    }
+
+
+
+    @DisplayName("Get request to /employees and only employees who works as a IT_PROG")
+    @Test
+    public void test2(){
+
+        Response response = given().accept(ContentType.JSON).log().all()
+                .queryParams("q","{\"job_id\": \"IT_PROG\"}")
+                .when().get("/employees") ;
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
         response.prettyPrint();
+
+
     }
 
 
