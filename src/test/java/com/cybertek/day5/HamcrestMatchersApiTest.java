f451e29ac10a5f920cbcad5002c08991deffc0de
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-22 16:22:13 +0200

    Hamcrest Matchers Api Test2

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -24,7 +24,38 @@ public class HamcrestMatchersApiTest {
                 .and()
                 .body("id", equalTo(15),
                         "name", is("Meta"),
-                        "gender",is("Female"),
+                        "gender", is("Female"),
                         "phone", is(1938695106));
     }
+
+    @DisplayName("CBTraining Teacher ")
+    @Test
+    public void test2() {
+
+        given().accept(ContentType.JSON).pathParam("id", 20450)
+                .when().get("http://api.cybertektraining.com/teacher/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                .and()
+                .header("Content-Length", is("236"))
+                .and()
+                .header("Date", notNullValue())
+                .and().assertThat()
+                .body("teachers[0].firstName", is("Alexander"),"teachers[0].lastName",is("Syrup"))
+                .body("teachers[0].gender",equalTo("male"));
+    }
+
+    @DisplayName("GET request to teacher/all and chaining")
+    @Test
+    public void test3() {
+
+        //verify Alexander, Darleen,Sean inside all teachers
+        given().accept(ContentType.JSON).get("http://api.cybertektraining.com/teacher/all")
+                .then().statusCode(200)
+                .body("teachers.firstName", hasItems("Alexander"));
+
+    }
+
 }
