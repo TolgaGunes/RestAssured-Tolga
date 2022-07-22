01cf0e773c62e3a5d23c3715a554d22d0441b8d4
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-22 21:36:32 +0200

    ORDSHamcrest Test

---
+++ src/test/java/com/cybertek/day5/ORDSHamcrestTest.java
@@ -0,0 +1,58 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.HRTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ORDSHamcrestTest extends HRTestBase {
+
+    @DisplayName("GET request to Employees IT_PROG endpoint and chaining")
+    @Test
+    public void employeesTest() {
+        //send a get request to employees endpoint with query parameter job_id IT_PROG
+        //verify each job_id is IT_PROG
+        //verify first names are... (find proper method to check list against list)
+        //verify emails without checking order (provide emails in different order,just make sure it has same emails)
+        //expected names
+
+        List<String> names = Arrays.asList("Alexander", "Bruce", "David", "Valli","Diana");
+                given().accept(ContentType.JSON).queryParam("q", "{\"job_id\": \"IT_PROG\"}")
+                        .get("/employees")
+                        .then().statusCode(200)
+                        .body("items.job_id", everyItem(equalTo("IT_PROG")))
+                        .body("items.first_name", containsInRelativeOrder("Alexander", "Bruce", "David", "Valli", "Diana"))
+                        .body("items.email", containsInAnyOrder("VPATABAL", "DAUSTIN", "BERNST", "AHUNOLD", "DLORENTZ"))
+                        .body("items.first_name", equalTo(names));
+    }
+
+    @Test
+    public void employeesTest2() {
+         //WE WANT TO CHAİN AND ALSO GET RESPONSE OBJECT
+
+            JsonPath jsonPath = given().accept(ContentType.JSON).queryParam("q", "{\"job_id\": \"IT_PROG\"}")
+                .get("/employees")
+                .then().statusCode(200)
+                .body("items.job_id", everyItem(equalTo("IT_PROG")))
+                .extract().jsonPath();
+
+             // extract method allows us to get response object after we ue then() method
+             //assert that we have only 5 firstName
+             assertThat(jsonPath.getList("items.first_name"), hasSize(5));
+
+             //assert firstnames order
+            assertThat(jsonPath.getList("items.first_name"),containsInRelativeOrder("Alexander","Bruce", "David"));
+
+    }
+
+
+}
