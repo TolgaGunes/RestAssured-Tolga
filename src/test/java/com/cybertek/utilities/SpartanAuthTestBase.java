e7a9ef04765d950330ce2cb5f9d108074c3d891c
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-07-28 13:55:04 +0200

    testAdmin & testEditorDelete was conducted.

--- src/test/java/com/cybertek/utilities/SpartanAuthTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanAuthTestBase.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.baseURI;
 public class SpartanAuthTestBase {
 
     @BeforeAll
-    public void init() {
+    public static void init() {
     //save base url inside this variable so that we do not need to type each http method
 
         baseURI= "http://3.80.210.194:7000/";
