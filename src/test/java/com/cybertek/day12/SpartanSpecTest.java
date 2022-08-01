ebd7817806db12ab19cdc38fefadda8b10d4a477
tolgagunes87@gmail.com <97402570+TolgaGunes@users.noreply.github.com>
2022-08-01 22:29:34 +0200

    SpartanNewBase

---
+++ src/test/java/com/cybertek/day12/SpartanSpecTest.java
@@ -0,0 +1,12 @@
+package com.cybertek.day12;
+
+import com.cybertek.utilities.SpartanNewBase;
+import io.restassured.specification.RequestSpecification;
+
+public class SpartanSpecTest extends SpartanNewBase {
+
+
+
+
+
+}
